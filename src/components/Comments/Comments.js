12c756809ec11a83a2c013cda79da7acfe6a64b8
Caleb Eno <ceno@icct.com>
2016-07-26 21:23:53 -0400

    Finished tutorial

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,21 +1,34 @@
 import React from 'react';
 
 class Comments extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.renderComponent = this.renderComponent.bind(this);
-    // }
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.renderComponent = this.renderComponent.bind(this);
+    }
 
     renderComponent(comment, i) {
+        const { removeComment, params } = this.props;
         return (
             <div className="comment" key={i}>
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
-                    <button className="remove-comment">&times;</button>
+                    <button className="remove-comment"
+                            onClick={removeComment.bind(null, params.postId, i)}>&times;</button>
                 </p>
             </div>
         );
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { postId } = this.props.params;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        this.props.addComment(postId, author, comment);
+        this.refs.comment.value = '';
+        this.refs.comment.focus();
     }
 
     render() {
@@ -23,7 +36,7 @@ class Comments extends React.Component {
         return (
             <div className="comment">
                 {postComments.map(this.renderComponent)}
-                <form className="comment-form" ref="commentForm">
+                <form className="comment-form" ref="commentForm" onSubmit={this.handleSubmit}>
                     <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,47 @@
-export default function comments(state = [], action) {
-    // console.log(state, action);
-    return state;
-}
+// a reducer takes in two things
+// 1. the action (info about what happened)
+// 2. copy of current state
 
+import * as types from '../constants/actionTypes';
+
+export default function comments(state = [], action) {
+
+    const addComment = (state, action) => {
+        // return the new state with the new comment
+        return [
+            ...state,
+            {
+                user: action.author,
+                text: action.comment
+            }
+        ];
+    };
+
+    const removeComment = (state, action) => {
+        return [
+            // from start to the one we want to delete
+            ...state.slice(0, action.index),
+            // after the one we want to delete
+            ...state.slice(action.index + 1)
+        ];
+    };
+
+    // contains all acceptable actions
+    const acceptedActions = {
+        [types.ADD_COMMENT]: addComment,
+        [types.REMOVE_COMMENT]: removeComment
+    };
+
+    // return state as it was if no accepted action
+    if (typeof acceptedActions[action.type] === 'undefined') {
+        return state;
+    }
+
+    // Always do this
+    return {
+        // take the current state
+        ...state,
+        // overwrite this post with a new one
+        [action.postId]: acceptedActions[action.type](state[action.postId], action)
+    };
+}
